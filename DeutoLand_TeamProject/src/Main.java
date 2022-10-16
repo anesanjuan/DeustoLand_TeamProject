9caa9adf0ec07e969745262869a0e5ddc830d582
mariaperaleseguiluz <mariaperaleseguiluz@localhost>
2022-10-16 11:27:49 +0200

    he dado a intro sin querer, estaba revisando lo que tenemos hecho hasta ahora

--- DeutoLand_TeamProject/src/Main.java
+++ DeutoLand_TeamProject/src/Main.java
@@ -4,6 +4,7 @@ import Ventanas.VentanaInicioSesion;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		GestorLN gln = new GestorLN(); 	
 		VentanaInicioSesion vi = new VentanaInicioSesion( gln );
 		vi.setVisible(true);
