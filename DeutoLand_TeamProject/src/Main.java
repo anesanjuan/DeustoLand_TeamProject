64a0bea09edd40381ee707d84898f89c4095b0ac
mariaperaleseguiluz <mariaperaleseguiluz@localhost>
2022-10-20 12:26:08 +0200

    Ventana Principal creada, con panel, label, button, etc.

--- DeutoLand_TeamProject/src/Main.java
+++ DeutoLand_TeamProject/src/Main.java
@@ -1,19 +1,23 @@
 import DeustoLand.GestorLN;
 import Ventanas.VentanaInicioSesion;
+import Ventanas.VentanaPrincipal;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		GestorLN gln = new GestorLN(); 	
-		VentanaInicioSesion vi = new VentanaInicioSesion( gln );
-		vi.setVisible(true);
+		//VentanaInicioSesion vi = new VentanaInicioSesion( gln );
+		//vi.setVisible(true);
 		
 		//VentanaFestival vf = new VentanaFestival();
 		//vf.setVisible(true);
 		
 		//VentanaRegistro vr = new VentanaRegistro(gln);
 		//vr.setVisible(true);
+		
+		VentanaPrincipal vp = new VentanaPrincipal( gln );
+		vp.setVisible(true);
 	}
 
 }
