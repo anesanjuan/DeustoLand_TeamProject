284f0df4bb06b45722c0f17ddb24d135e29b281b
mariaperaleseguiluz <mariaperaleseguiluz@localhost>
2023-01-19 14:49:25 +0100

    bd

--- DeutoLand_TeamProject/src/Ventanas/Renderer.java
+++ DeutoLand_TeamProject/src/Ventanas/Renderer.java
@@ -21,9 +21,12 @@ public class Renderer extends JLabel implements TableCellRenderer {
 
 	private TipoEntrada tipoE = null;
 	private ArrayList<Integer> is=null;
+	
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		
 		this.setText(value.toString());
 		this.setFont(new Font("Helvetica Bold", Font.PLAIN, 12));
+		
 		if(tipoE!=null) {
 			TipoEntrada tipoV = (TipoEntrada) table.getModel().getValueAt(row, 3);
 			if (tipoV.equals(tipoE)) {
